999051904327ddb3942a9eddab33d4deb0ad7f7a
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-08-19 19:54:18 +0600

    vercel url 3rd update fix

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:8000/api/login/', {
+            const response = await axios.post('https://point-of-sale-sable.vercel.app/api/login/', {
                 username,
                 password
             });
