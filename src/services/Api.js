8f27b4f52eac6e903a3f62df523b9e68afba8bbf
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-08-24 18:26:26 +0600

    link conflict

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// const API_URL = 'http://127.0.0.1:8000/api/';
 const API_URL = 'http://127.0.0.1:8000/api/';
 
 export const fetchSuppliers = () => axios.get(`${API_URL}suppliers/`);
